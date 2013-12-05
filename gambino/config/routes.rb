cff9cfd8c216bdb841ad2bbe341931f0e89665c3
niccs14 <niccs14@yahoo.com>
2013-12-04 20:33:47 -0800

    finished instant gratification

--- gambino/config/routes.rb
+++ gambino/config/routes.rb
@@ -1,4 +1,6 @@
 Gambino::Application.routes.draw do
+  get "say/hello"
+  get "say/goodbye"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
