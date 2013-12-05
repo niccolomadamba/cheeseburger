66d2371c6d569cbd49aa4d0fbb9034352e77a28c
niccs14 <niccs14@yahoo.com>
2013-12-04 19:29:15 -0800

    rails added

---
+++ gambino/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gambino::Application.config.secret_key_base = 'fb22e1663e92f26781c5ad33332dd8e13d319201385839c8931d32b21220164cd2635622b020b5e4bc9c7e33aad88d851a5b7b22b4565c8ebb865bd2221ea909'
